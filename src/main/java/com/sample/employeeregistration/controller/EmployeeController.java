7f36028b05876a8496414d1ca972cb9d18fc315d
bedrettinerkan <bedrettinerkan123@gmail.com>
2022-09-22 20:04:03 +0300

    initial commit.

--- src/main/java/com/sample/employeeregistration/controller/EmployeeController.java
+++ src/main/java/com/sample/employeeregistration/controller/EmployeeController.java
@@ -4,8 +4,14 @@ import com.sample.employeeregistration.bootstrap.DataGenerator;
 import com.sample.employeeregistration.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import javax.xml.crypto.Data;
 
 @Controller
 @RequestMapping("/employee")
@@ -19,4 +25,20 @@ public class EmployeeController {
         return "employee/employee-create";
     }
 
+    @PostMapping("/list")
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            model.addAttribute("states", DataGenerator.getAllStates());
+
+            return "employee/employee-create";
+
+        }
+
+        DataGenerator.saveEmployee(employee);
+
+        model.addAttribute("employees",DataGenerator.readAllEmployees());
+
+        return "employee/employee-list";
+    }
 }
--- src/main/java/com/sample/employeeregistration/model/Employee.java
+++ src/main/java/com/sample/employeeregistration/model/Employee.java
@@ -1,7 +1,12 @@
 package com.sample.employeeregistration.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -10,9 +15,16 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @ToString
 public class Employee {
+    //@NotNull    // Except null, anything can be accepted
+    //@NotEmpty   // Except null and empty String anything can be accepted
+    //@NotBlank   // Except null, empty String and only space anything can be accepted.
 
+    @NotBlank
+    @Size(max = 12,min = 2)
     private String firstName;
     private String lastName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     private String email;
     private String password;
